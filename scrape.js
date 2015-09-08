a59580821fb10169a69441926612acc4a0e15525
Satya Brat Singh <satya.singh@flipkart.com>
2015-09-09 01:40:06 +0530

    Add scrape files

---
+++ scrape.js
@@ -0,0 +1,24 @@
+#$.ajax({
+# url: "http://stackoverflow.com/questions/26274544/convert-command-curl-to-javascript",
+# type: 'GET',
+# success: function (data) {
+#  alert(JSON.stringify(data));
+#},
+#  error: function(){
+#   alert("Cannot get data");
+# }
+#});
+
+function getLoc() { 
+   var metas = document.getElementsByTagName('meta'); 
+   alert(metas)
+   alert("Lol")
+
+   // for (i=0; i<metas.length; i++) { 
+   //    if (metas[i].getAttribute("property") == "video") { 
+   //       return metas[i].getAttribute("content"); 
+   //    } 
+   // } 
+
+   //  return "";
+} 

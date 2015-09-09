4f9570cc5f63c5d4a1de4035b3b4d404e6b6d8f0
Sumesh P <sumeshpremraj@gmail.com>
2015-09-09 22:04:29 +0530

    Forgot to add new JS file lol

--- scrape_jquery.js
+++ scrape_jquery.js
@@ -6,16 +6,10 @@ function getDestinationCordinates() {
     var div = document.getElementById('display');
 
     // Get all meta tags, check property and store lat/long into JS vars
-    var metas = document.getElementsByTagName('meta'); 
-     
-    for (i=0; i<metas.length; i++) {    
-        if (metas[i].getAttribute("property") == "zomatocom:location:latitude") { 
-                    var lat = metas[i].getAttribute("content");
-                    $("#display").append("Latitude: "+lat);
-            }
-            else if (metas[i].getAttribute("property") == "zomatocom:location:longitude") { 
-                    var long = metas[i].getAttribute("content");
-                    $("#display").append("<br/>Longitude: "+long);
-            }
-    }
+    //var metas = document.getElementsByTagName('meta'); 
+
+    var lat = $("meta[property='zomatocom:location:latitude']").attr("content");
+    var long = $("meta[property='zomatocom:location:longitude']").attr("content");
+    $("#display").append("Latitude: "+lat); 
+    $("#display").append("<br/>Longitude: "+long);
 } 

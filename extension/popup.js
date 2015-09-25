60d4bd66fd4d8eb716d84321839d6ae270566a9f
Satya Brat Singh <satya.singh@flipkart.com>
2015-09-26 01:49:17 +0530

    ajax implementaion

--- extension/popup.js
+++ extension/popup.js
@@ -4,12 +4,35 @@ function onPageDetailsReceived(pageDetails)  {
     document.getElementById('latitude').value = pageDetails.latitude; 
     document.getElementById('longitude').value = pageDetails.longitude;
     document.getElementById('myLat').value = pageDetails.myLat; 
+    myLat=pageDetails.myLat;
     document.getElementById('myLong').value = pageDetails.myLong;
+    myLong=pageDetails.myLong;
 
+    // get cookie named logged_in for domain .uber.com
+    chrome.cookies.getAll({"name": "logged_in", "domain": ".uber.com"},
+        // callback function to be called when getAll() returns
+        function(cookie) {
+            // if cookie exists and it's value is true
+            if ((cookie.length) && (!cookie[0].value.localeCompare("true"))) {    // localeCompare returns 0 when strings are equal
+                //alert("logged in");
+                var product_result=getProducts();
+                //alert(product_result);
+                statusDisplay.innerHTML=product_result;
+            }
+            else{
+                alert("logged out");
+                chrome.tabs.create({'url': 'https://ola-app.herokuapp.com/', }, function(window) {});
+            }
+        } 
+    );
 } 
 
 // Global reference to the status display SPAN
 var statusDisplay = null;
+var latitude;
+var longitude;
+var myLat;
+var myLong;
 
 // POST the data to the server using XMLHttpRequest
 function bookCab() {
@@ -58,26 +81,38 @@ function bookCab() {
 
     // // Send the request and set status
     // xhr.send(params);
-    statusDisplay.innerHTML = 'Saving...';
+    //statusDisplay.innerHTML = 'Saving...';
+}
+
+function getProducts(){
+    var postUrl = 'http://localhost:7000/products';
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', postUrl, false);
+    xhr.setRequestHeader('Content-type', 'application/json');
+
+    xhr.onreadystatechange = function() { 
+        // If the request completed
+        if (xhr.readyState == 4) {
+            statusDisplay.innerHTML = '';
+            if (xhr.status == 200) {
+                // If it was a success, close the popup after a short delay
+                statusDisplay.innerHTML=JSON.parse(xhr.responseText).products[0].capacity;
+                alert(JSON.parse(xhr.responseText).products[0].capacity);
+                //window.setTimeout(window.close, 1000);
+            } else {
+                // Show what went wrong
+                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
+            }
+        }
+    };
+
+    // Send the request and set status
+    xhr.send(JSON.stringify({"start_latitude":12.957067,"start_longitude":77.699449}));
+
 }
 
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
-
-    // get cookie named logged_in for domain .uber.com
-    chrome.cookies.getAll({"name": "logged_in", "domain": ".uber.com"},
-        // callback function to be called when getAll() returns
-        function(cookie) {
-            // if cookie exists and it's value is true
-            if ((cookie.length) && (!cookie[0].value.localeCompare("true"))) {    // localeCompare returns 0 when strings are equal
-                alert("logged in");
-            }
-            else{
-                alert("logged out");
-                chrome.tabs.create({'url': 'https://ola-app.herokuapp.com/', }, function(window) {});
-            }
-        } 
-    );
     
     // Cache a reference to the status display SPAN
     console.log("in popup.js page");
